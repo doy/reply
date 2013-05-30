69e4ff7c28debe5cae5bd505d5df12bd12bdd11a
Jesse Luehrs <doy@tozt.net>
2013-05-30 04:26:53 -0500

    LoadClass plugin

---
+++ lib/App/REPL/Plugin/LoadClass.pm
@@ -0,0 +1,28 @@
+package App::REPL::Plugin::LoadClass;
+use strict;
+use warnings;
+
+use base 'App::REPL::Plugin';
+
+use Module::Runtime 'use_package_optimistically';
+use Try::Tiny;
+
+sub execute {
+    my $self = shift;
+    my ($next, @args) = @_;
+
+    try {
+        $next->(@args);
+    }
+    catch {
+        if (/^Can't locate object method "[^"]*" via package "([^"]*)"/) {
+            use_package_optimistically($1);
+            $next->(@args);
+        }
+        else {
+            die $_;
+        }
+    }
+}
+
+1;
