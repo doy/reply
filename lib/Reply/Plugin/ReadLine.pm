b9ed547db043ef57dca6d2d633c344bf0a00af2c
Jesse Luehrs <doy@tozt.net>
2013-05-30 05:20:24 -0500

    save readline history

--- lib/Reply/Plugin/ReadLine.pm
+++ lib/Reply/Plugin/ReadLine.pm
@@ -4,13 +4,29 @@ use warnings;
 
 use base 'Reply::Plugin';
 
+use File::HomeDir;
+use File::Spec;
 use Term::ReadLine;
+
+my $history = File::Spec->catfile(File::HomeDir->my_data, '.reply_history');
 
 sub new {
     my $class = shift;
 
     my $self = $class->SUPER::new(@_);
     $self->{term} = Term::ReadLine->new('Reply');
+
+    if (open my $fh, '<', $history) {
+        for my $line (<$fh>) {
+            chomp $line;
+            $self->{term}->addhistory($line);
+        }
+    }
+    else {
+        my $e = $!;
+        warn "Couldn't open $history for reading: $e"
+            if -e $history;
+    }
 
     return $self;
 }
@@ -20,6 +36,16 @@ sub read_line {
     my ($next, $prompt) = @_;
 
     return $self->{term}->readline($prompt);
+}
+
+sub DESTROY {
+    my $self = shift;
+
+    # XXX support more later
+    return unless $self->{term}->ReadLine eq 'Term::ReadLine::Gnu';
+
+    $self->{term}->WriteHistory($history)
+        or warn "Couldn't write history to $history";
 }
 
 1;
