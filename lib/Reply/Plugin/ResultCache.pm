bc5ef3860cd51aedff32d087ffcebbf80fc026d3
Jesse Luehrs <doy@tozt.net>
2013-05-31 03:07:12 -0500

    result cache plugin

---
+++ lib/Reply/Plugin/ResultCache.pm
@@ -0,0 +1,41 @@
+package Reply::Plugin::ResultCache;
+use strict;
+use warnings;
+
+use base 'Reply::Plugin';
+
+sub new {
+    my $class = shift;
+
+    my $self = $class->SUPER::new(@_);
+    $self->{results} = [];
+
+    return $self;
+}
+
+sub compile {
+    my $self = shift;
+    my ($next, $line, %args) = @_;
+
+    $args{environment} ||= {};
+    $args{environment}{'@res'} = $self->{results};
+
+    $next->($line, %args);
+}
+
+sub execute {
+    my $self = shift;
+    my ($next, @args) = @_;
+
+    my @res = $next->(@args);
+    if (@res == 1) {
+        push @{ $self->{results} }, $res[0];
+    }
+    elsif (@res > 1) {
+        push @{ $self->{results} }, \@res;
+    }
+
+    return @res;
+}
+
+1;
