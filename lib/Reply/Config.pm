41060e3edf0b34aa0ed8bce3fe27f47377fa9a85
Jesse Luehrs <doy@tozt.net>
2013-06-08 15:10:21 -0500

    document Reply::Config

--- lib/Reply/Config.pm
+++ lib/Reply/Config.pm
@@ -1,10 +1,41 @@
 package Reply::Config;
 use strict;
 use warnings;
+# ABSTRACT: config loading for Reply
 
 use Config::INI::Reader::Ordered;
 use File::HomeDir;
 use File::Spec;
+
+=head1 SYNOPSIS
+
+  use Reply;
+  use Reply::Config;
+
+  Reply->new(config => Reply::Config->new(file => 'something_else'))->run;
+
+=head1 DESCRIPTION
+
+This class abstracts out the config file loading, so that other applications
+can start up Reply shells using similar logic. Reply configuration is specified
+in an INI format - see L<Reply> for more details.
+
+=cut
+
+=method new(%opts)
+
+Creates a new config object. Valid options are:
+
+=over 4
+
+=item file
+
+Configuration file to use. If the file is specified by a relative path, it will
+be relative to the user's home directory, otherwise it will be used as-is.
+
+=back
+
+=cut
 
 sub new {
     my $class = shift;
@@ -28,7 +59,19 @@ sub new {
     return $self;
 }
 
+=method file
+
+Returns the absolute path to the config file that is to be used.
+
+=cut
+
 sub file { shift->{file} }
+
+=method data
+
+Returns the loaded configuration data.
+
+=cut
 
 sub data {
     my $self = shift;
