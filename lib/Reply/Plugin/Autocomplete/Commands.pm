5b9076b09b5da1f415299309f16dff21b54a62f0
Jesse Luehrs <doy@tozt.net>
2013-07-10 02:45:48 -0400

    docs

--- lib/Reply/Plugin/Autocomplete/Commands.pm
+++ lib/Reply/Plugin/Autocomplete/Commands.pm
@@ -1,8 +1,21 @@
 package Reply::Plugin::Autocomplete::Commands;
 use strict;
 use warnings;
+# ABSTRACT: tab completion for reply commands
 
 use base 'Reply::Plugin';
+
+=head1 SYNOPSIS
+
+  ; .replyrc
+  [ReadLine]
+  [Autocomplete::Commands]
+
+=head1 DESCRIPTION
+
+This plugin registers a tab key handler to autocomplete Reply commands.
+
+=cut
 
 sub tab_handler {
     my $self = shift;
